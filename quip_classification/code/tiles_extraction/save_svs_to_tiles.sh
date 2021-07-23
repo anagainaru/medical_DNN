7fa93cbfd1e3b52e675f14e52f9d0a2507fdadbf
Ana Gainaru <ana.gainaru@gmail.com>
2021-07-22 20:42:23 -0400

    Code for tiles extraction

---
+++ quip_classification/code/tiles_extraction/save_svs_to_tiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+source ../conf/variables.sh
+
+set -x
+COD_PARA=$1
+MAX_PARA=$2
+IN_FOLDER=${SVS_INPUT_PATH}
+OUT_FOLDER=${PATCH_PATH}
+echo $COD_PARA
+echo $MAX_PARA
+echo $IN_FOLDER
+echo $OUT_FOLDER
+
+
+LINE_N=0
+for files in ${IN_FOLDER}/*.*; do
+    LINE_N=$((LINE_N+1))
+    if [ $((LINE_N % MAX_PARA)) -ne ${COD_PARA} ]; then continue; fi
+
+    SVS=`echo ${files} | awk -F'/' '{print $NF}'`
+    echo "[debug] Looking at file $SVS"
+    python save_svs_to_tiles.py $SVS $IN_FOLDER $OUT_FOLDER
+    if [ $? -ne 0 ]; then
+        echo "failed extracting patches for " ${SVS}
+        rm -rf ${OUT_FOLDER}/${SVS}
+    fi
+done
+set +x
+exit 0;
+
+
---
+++ quip_classification/code/tiles_extraction/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+source ../conf/variables.sh
+
+THREAD=$1
+NTHREADS=$2
+
+bash save_svs_to_tiles.sh ${THREAD}  ${NTHREADS} 2>&1 # > ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_${THREAD} 2>&1
+#bash save_svs_to_tiles.sh 0 1  > ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_0.txt 2>&1
+#bash save_svs_to_tiles.sh 0 6  > ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_0.txt 2>&1
+#bash save_svs_to_tiles.sh 1 6  > ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_1.txt 2>&1
+#bash save_svs_to_tiles.sh 2 6  > ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_2.txt 2>&1
+#bash save_svs_to_tiles.sh 3 6  > ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_3.txt 2>&1
+#bash save_svs_to_tiles.sh 4 6  > ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_3.txt 2>&1
+#bash save_svs_to_tiles.sh 5 6  > ${LOG_OUTPUT_FOLDER}/log.save_svs_to_tiles.thread_3.txt 2>&1
+#wait
+set +x 
+exit 0
